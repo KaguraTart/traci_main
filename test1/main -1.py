0cebb0c5ada950b113ca98dca082d3b1e4269734
Kagura Tart <diantaodeng@163.com>
2021-11-24 17:09:47 +0800

    123

--- test1/main -1.py
+++ test1/main -1.py
@@ -74,7 +74,7 @@ def traci_control_env_update(step_time):
 
 if __name__ == "__main__":
 
-    N_STATES = 600
+    N_STATES = 100
 
     print('------------------------------------------------')
     a = traci_control_env_update(N_STATES)
